adebc00e4f9d1697771cf2a5b5b09668bf117ae5
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2022-07-26 01:12:27 -0300

    Agregando namespaces

--- Editor/Algebra/MatrizDiscreta.cs
+++ Editor/Algebra/MatrizDiscreta.cs
@@ -2,165 +2,169 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MatrizDiscreta : IMatriz
+namespace ItIsNotOnlyMe.LinealSolver
 {
-    private static float epsilon = 0.001f;
-
-    private Dictionary<uint, float> _matriz;
-    private uint _tamanioX, _tamanioY;
-
-    public MatrizDiscreta(uint tamanioX, uint tamanioY)
+    public class MatrizDiscreta : IMatriz
     {
-        _matriz = new Dictionary<uint, float>();
-        _tamanioX = tamanioX;
-        _tamanioY = tamanioY;
-    }
+        private static float epsilon = 0.001f;
 
-    public float this[uint i, uint j]
-    {
-        get
+        private Dictionary<uint, float> _matriz;
+        private uint _tamanioX, _tamanioY;
+
+        public MatrizDiscreta(uint tamanioX, uint tamanioY)
         {
-            uint posicion = i * _tamanioX + j;
-            return _matriz.ContainsKey(posicion) ? _matriz[posicion] : 0;
-        }
-        set
-        {
-            uint posicion = i * _tamanioX + j;
-            if (_matriz.ContainsKey(posicion))
-                _matriz[posicion] = value;
-            else
-                _matriz.Add(posicion, value);            
-        }
-    }
-
-    public bool EsCuadrada() => _tamanioX == _tamanioY;
-    public bool SonMultiplicables(IMatriz otro) => _tamanioY == otro.Tamanio().Item1;
-
-    public Tuple<uint, uint> Tamanio() => new Tuple<uint, uint>(_tamanioX, _tamanioY);
-
-    public IMatriz Transponer()
-    {
-        MatrizDiscreta matrizTranspuesta = new MatrizDiscreta(_tamanioY, _tamanioX);
-
-        foreach (KeyValuePair<uint, float> keyValue in _matriz)
-        {
-            uint i = keyValue.Key / _tamanioX;
-            uint j = keyValue.Key % _tamanioX;
-
-            matrizTranspuesta[j, i] = keyValue.Value;
+            _matriz = new Dictionary<uint, float>();
+            _tamanioX = tamanioX;
+            _tamanioY = tamanioY;
         }
 
-        return matrizTranspuesta;
-    }
-
-    public IMatriz Multiplicar(IMatriz otro)
-    {
-        if (!SonMultiplicables(otro))
-            return null;
-
-        uint tamanioFinalX = _tamanioX, tamanioFinalY = otro.Tamanio().Item2;
-        Matriz resultado = new Matriz(tamanioFinalX, tamanioFinalY);
-
-        uint cantidadIguales = _tamanioY;
-        for (uint i = 0; i < tamanioFinalX; i++)
-            for (uint j = 0; j < tamanioFinalY; j++)
+        public float this[uint i, uint j]
+        {
+            get
             {
-                for (uint k = 0; k < cantidadIguales; k++)
-                    resultado[i, j] += this[i, k] * otro[k, j];
+                uint posicion = i * _tamanioX + j;
+                return _matriz.ContainsKey(posicion) ? _matriz[posicion] : 0;
+            }
+            set
+            {
+                uint posicion = i * _tamanioX + j;
+                if (_matriz.ContainsKey(posicion))
+                    _matriz[posicion] = value;
+                else
+                    _matriz.Add(posicion, value);
+            }
+        }
+
+        public bool EsCuadrada() => _tamanioX == _tamanioY;
+        public bool SonMultiplicables(IMatriz otro) => _tamanioY == otro.Tamanio().Item1;
+
+        public Tuple<uint, uint> Tamanio() => new Tuple<uint, uint>(_tamanioX, _tamanioY);
+
+        public IMatriz Transponer()
+        {
+            MatrizDiscreta matrizTranspuesta = new MatrizDiscreta(_tamanioY, _tamanioX);
+
+            foreach (KeyValuePair<uint, float> keyValue in _matriz)
+            {
+                uint i = keyValue.Key / _tamanioX;
+                uint j = keyValue.Key % _tamanioX;
+
+                matrizTranspuesta[j, i] = keyValue.Value;
             }
 
-        return resultado;
-    }
-
-    public IMatriz Sumar(IMatriz otro)
-    {
-        if (!SonDelMismoTamanio(otro))
-            return null;
-
-        Matriz resultado = new Matriz(_tamanioX, _tamanioY);
-
-        for (uint i = 0; i < _tamanioX; i++)
-            for (uint j = 0; j < _tamanioY; j++)
-                resultado[i, j] = otro[i, j];
-
-        foreach (KeyValuePair<uint, float> keyValue in _matriz)
-        {
-            uint i = keyValue.Key / _tamanioX;
-            uint j = keyValue.Key % _tamanioX;
-
-            resultado[i, j] += keyValue.Value;
+            return matrizTranspuesta;
         }
 
-        return resultado;
-    }
-
-    private bool SonDelMismoTamanio(IMatriz otro)
-    {
-        return Tamanio().Item1 == otro.Tamanio().Item1 && Tamanio().Item2 == Tamanio().Item2;
-    }
-
-    public IMatriz Restar(IMatriz otro)
-    {
-        return Sumar(otro.Multiplicar(-1));
-    }
-
-    public IMatriz Multiplicar(float valor)
-    {
-        MatrizDiscreta resultado = new MatrizDiscreta(_tamanioX, _tamanioY);
-
-        foreach (KeyValuePair<uint, float> keyValue in _matriz)
+        public IMatriz Multiplicar(IMatriz otro)
         {
-            uint i = keyValue.Key / _tamanioX;
-            uint j = keyValue.Key % _tamanioX;
+            if (!SonMultiplicables(otro))
+                return null;
 
-            resultado[i, j] = keyValue.Value * valor;
+            uint tamanioFinalX = _tamanioX, tamanioFinalY = otro.Tamanio().Item2;
+            Matriz resultado = new Matriz(tamanioFinalX, tamanioFinalY);
+
+            uint cantidadIguales = _tamanioY;
+            for (uint i = 0; i < tamanioFinalX; i++)
+                for (uint j = 0; j < tamanioFinalY; j++)
+                {
+                    for (uint k = 0; k < cantidadIguales; k++)
+                        resultado[i, j] += this[i, k] * otro[k, j];
+                }
+
+            return resultado;
         }
 
-        return resultado;
-    }
-
-    public float ModuloInfinito()
-    {
-        Vector resultado = new Vector(_tamanioX);
-        foreach (KeyValuePair<uint, float> keyValue in _matriz)
+        public IMatriz Sumar(IMatriz otro)
         {
-            uint i = keyValue.Key / _tamanioX;
-            resultado[i] += Mathf.Abs(keyValue.Value);
-        }
-        return resultado.ModuloInfinito();
-    }
+            if (!SonDelMismoTamanio(otro))
+                return null;
 
-    public bool EsSimetrica()
-    {
-        if (!EsCuadrada())
-            return false;
+            Matriz resultado = new Matriz(_tamanioX, _tamanioY);
 
-        bool esSimetrica = true;
-        foreach (KeyValuePair<uint, float> keyValue in _matriz)
-        {
-            uint i = keyValue.Key / _tamanioX;
-            uint j = keyValue.Key % _tamanioX;
+            for (uint i = 0; i < _tamanioX; i++)
+                for (uint j = 0; j < _tamanioY; j++)
+                    resultado[i, j] = otro[i, j];
 
-            esSimetrica &= (keyValue.Value < this[j, i] + epsilon && keyValue.Value > this[j, i] - epsilon);
+            foreach (KeyValuePair<uint, float> keyValue in _matriz)
+            {
+                uint i = keyValue.Key / _tamanioX;
+                uint j = keyValue.Key % _tamanioX;
+
+                resultado[i, j] += keyValue.Value;
+            }
+
+            return resultado;
         }
 
-        return esSimetrica;
-    }
-
-    public bool EsDiagonalmenteDominante()
-    {
-        if (!EsCuadrada())
-            return false;
-
-        bool esDiagonalmenteDominante = true;
-        for (uint i = 0; i < _tamanioX && esDiagonalmenteDominante; i++)
+        private bool SonDelMismoTamanio(IMatriz otro)
         {
-            float valor = 0;
-            for (uint j = 0; j < _tamanioY && esDiagonalmenteDominante; j++)
-                valor += (i == j) ? 0 : Mathf.Abs(this[i, j]);
-            esDiagonalmenteDominante &= Mathf.Abs(this[i, i]) > valor;
+            return Tamanio().Item1 == otro.Tamanio().Item1 && Tamanio().Item2 == Tamanio().Item2;
         }
 
-        return esDiagonalmenteDominante;
+        public IMatriz Restar(IMatriz otro)
+        {
+            return Sumar(otro.Multiplicar(-1));
+        }
+
+        public IMatriz Multiplicar(float valor)
+        {
+            MatrizDiscreta resultado = new MatrizDiscreta(_tamanioX, _tamanioY);
+
+            foreach (KeyValuePair<uint, float> keyValue in _matriz)
+            {
+                uint i = keyValue.Key / _tamanioX;
+                uint j = keyValue.Key % _tamanioX;
+
+                resultado[i, j] = keyValue.Value * valor;
+            }
+
+            return resultado;
+        }
+
+        public float ModuloInfinito()
+        {
+            Vector resultado = new Vector(_tamanioX);
+            foreach (KeyValuePair<uint, float> keyValue in _matriz)
+            {
+                uint i = keyValue.Key / _tamanioX;
+                resultado[i] += Mathf.Abs(keyValue.Value);
+            }
+            return resultado.ModuloInfinito();
+        }
+
+        public bool EsSimetrica()
+        {
+            if (!EsCuadrada())
+                return false;
+
+            bool esSimetrica = true;
+            foreach (KeyValuePair<uint, float> keyValue in _matriz)
+            {
+                uint i = keyValue.Key / _tamanioX;
+                uint j = keyValue.Key % _tamanioX;
+
+                esSimetrica &= (keyValue.Value < this[j, i] + epsilon && keyValue.Value > this[j, i] - epsilon);
+            }
+
+            return esSimetrica;
+        }
+
+        public bool EsDiagonalmenteDominante()
+        {
+            if (!EsCuadrada())
+                return false;
+
+            bool esDiagonalmenteDominante = true;
+            for (uint i = 0; i < _tamanioX && esDiagonalmenteDominante; i++)
+            {
+                float valor = 0;
+                for (uint j = 0; j < _tamanioY && esDiagonalmenteDominante; j++)
+                    valor += (i == j) ? 0 : Mathf.Abs(this[i, j]);
+                esDiagonalmenteDominante &= Mathf.Abs(this[i, i]) > valor;
+            }
+
+            return esDiagonalmenteDominante;
+        }
     }
+
 }
--- Editor/LinealSolver.cs
+++ Editor/LinealSolver.cs
@@ -1,152 +1,146 @@
-
-using UnityEngine;
-
-public static class LinealSolver
+namespace ItIsNotOnlyMe.LinealSolver
 {
-    // matriz A tiene que ser simetrica y definida positiva
-    public static IMatriz GradienteConjugado(IMatriz matrizA, IMatriz vectorB, int cantidadDeIteracionesMaximas, float errorMaximo)
+    public static class LinealSolver
     {
-        if (!CondicionDeValidez(matrizA, vectorB))
-            return null;
-
-        uint tamanio = vectorB.Tamanio().Item1;
-
-        IMatriz x = new Vector(tamanio);
-        IMatriz r = vectorB.Restar(matrizA.Multiplicar(x));
-        IMatriz p = r;
-
-        int iteracionesActual = 0;
-        while (iteracionesActual < cantidadDeIteracionesMaximas)
+        // matriz A tiene que ser simetrica y definida positiva
+        public static IMatriz GradienteConjugado(IMatriz matrizA, IMatriz vectorB, int cantidadDeIteracionesMaximas, float errorMaximo)
         {
-            float rTransPorR = (r.Transponer()).Multiplicar(r)[0, 0];
-            float alfa = ((rTransPorR) / ((p.Transponer()).Multiplicar(matrizA)).Multiplicar(p)[0, 0]);
+            if (!CondicionDeValidez(matrizA, vectorB))
+                return null;
 
-            x = x.Sumar(p.Multiplicar(alfa));
-            r = r.Restar(matrizA.Multiplicar(p.Multiplicar(alfa)));
+            uint tamanio = vectorB.Tamanio().Item1;
 
-            if (r.ModuloInfinito() < errorMaximo)
-                break;
+            IMatriz x = new Vector(tamanio);
+            IMatriz r = vectorB.Restar(matrizA.Multiplicar(x));
+            IMatriz p = r;
 
-            float beta = (((r.Transponer()).Multiplicar(r)[0, 0]) / rTransPorR);
-            p = r.Sumar(p.Multiplicar(beta));
-
-            iteracionesActual++;
-        }
-
-        Debug.Log("Cantidad de iteracion: " + iteracionesActual + " en Gradiente conjugado");
-
-        return x;
-    }
-
-    // matriz A tiene que ser diagonalmente dominante
-    public static IMatriz Jacobi(IMatriz matrizA, IMatriz vectorB, int cantidadDeIteracionesMaximas, float errorMaximo)
-    {
-        if (!CondicionDeValidez(matrizA, vectorB))
-            return null;
-
-        uint tamanioX = matrizA.Tamanio().Item1, tamanioY = matrizA.Tamanio().Item2;
-        Matriz matrizE = new Matriz(tamanioX, tamanioY);
-        Vector vectorC = new Vector(tamanioX);
-
-        for (uint i = 0; i < tamanioX; i++)
-        {
-            for (uint j = 0; j < tamanioY; j++)
+            int iteracionesActual = 0;
+            while (iteracionesActual < cantidadDeIteracionesMaximas)
             {
-                if (i == j)
-                    continue;
-                matrizE[i, j] = -matrizA[i, j] / matrizA[i, i];
-            }
-            vectorC[i] = ((Vector)vectorB)[i] / matrizA[i, i];
-        }
+                float rTransPorR = (r.Transponer()).Multiplicar(r)[0, 0];
+                float alfa = ((rTransPorR) / ((p.Transponer()).Multiplicar(matrizA)).Multiplicar(p)[0, 0]);
 
-        IMatriz xAnterior = vectorC;
-        IMatriz x = OperacionIteracion(matrizE, vectorC, xAnterior);
+                x = x.Sumar(p.Multiplicar(alfa));
+                r = r.Restar(matrizA.Multiplicar(p.Multiplicar(alfa)));
 
-        int iteracionesActual = 1;
-        while (CalculoDelError(x, xAnterior) > errorMaximo && iteracionesActual < cantidadDeIteracionesMaximas)
-        {
-            xAnterior = x;
-            x = OperacionIteracion(matrizE, vectorC, x);
-            iteracionesActual++;
-        }
+                if (r.ModuloInfinito() < errorMaximo)
+                    break;
 
-        Debug.Log("Cantidad de iteracion: " + iteracionesActual + " en Jacobi");
+                float beta = (((r.Transponer()).Multiplicar(r)[0, 0]) / rTransPorR);
+                p = r.Sumar(p.Multiplicar(beta));
 
-        return x;
-    }
-
-    // matriz A tiene que ser diagonalmente dominante
-    public static IMatriz GaussSeidel(IMatriz matrizA, IMatriz vectorB, int cantidadDeIteracionesMaximas, float errorMaximo)
-    {
-        if (!CondicionDeValidez(matrizA, vectorB))
-            return null;
-
-        uint tamanio = matrizA.Tamanio().Item1;
-        Matriz matrizN = new Matriz(tamanio, tamanio);
-        Matriz matrizP = new Matriz(tamanio, tamanio);
-
-        for (uint i = 0; i < tamanio; i++)
-            for (uint j = 0; j < tamanio; j++)
-            {
-                if (i <= j)
-                    matrizN[i, j] = matrizA[i, j];
-                else
-                    matrizP[i, j] = -matrizA[i, j];
+                iteracionesActual++;
             }
 
-        Matriz matrizNInversa = MatrizTrianguladaSuperiorInversa(matrizN);
-
-        IMatriz matrizE = matrizNInversa.Multiplicar(matrizP);
-        IMatriz vectorC = matrizNInversa.Multiplicar(vectorB);
-
-        IMatriz xAnterior = vectorC;
-        IMatriz x = OperacionIteracion(matrizE, vectorC, xAnterior);
-
-        int iteracionesActual = 1;
-        while (CalculoDelError(x, xAnterior) > errorMaximo && iteracionesActual < cantidadDeIteracionesMaximas)
-        {
-            xAnterior = x;
-            x = OperacionIteracion(matrizE, vectorC, x);
-            iteracionesActual++;
+            return x;
         }
 
-        Debug.Log("Cantidad de iteracion: " + iteracionesActual + " en GaussSeidel");
+        // matriz A tiene que ser diagonalmente dominante
+        public static IMatriz Jacobi(IMatriz matrizA, IMatriz vectorB, int cantidadDeIteracionesMaximas, float errorMaximo)
+        {
+            if (!CondicionDeValidez(matrizA, vectorB))
+                return null;
 
-        return x;
-    }
+            uint tamanioX = matrizA.Tamanio().Item1, tamanioY = matrizA.Tamanio().Item2;
+            Matriz matrizE = new Matriz(tamanioX, tamanioY);
+            Vector vectorC = new Vector(tamanioX);
 
-    private static IMatriz OperacionIteracion(IMatriz matrizE, IMatriz vectorC, IMatriz vectorX)
-    {
-        return vectorC.Sumar(matrizE.Multiplicar(vectorX));
-    }
-
-    private static float CalculoDelError(IMatriz vectorX, IMatriz vectorXAnterior)
-    {
-        return (vectorX.Restar(vectorXAnterior)).ModuloInfinito();
-    }
-
-    private static Matriz MatrizTrianguladaSuperiorInversa(Matriz matriz)
-    {
-        uint tamanio = matriz.Tamanio().Item1;
-        Matriz matrizInversa = new Matriz(tamanio, tamanio);
-
-        for (int x = (int)(tamanio - 1); x >= 0; x--)
-            for (int y = (int)(tamanio - 1); y >= x; y--)
+            for (uint i = 0; i < tamanioX; i++)
             {
-                uint i = (uint)x, j = (uint)y;
-
-                matrizInversa[i, j] = 1 / matriz[j, j];
-                if (i != j)
-                    matrizInversa[i, j] *= -matriz[i, j] / matriz[i, i];
+                for (uint j = 0; j < tamanioY; j++)
+                {
+                    if (i == j)
+                        continue;
+                    matrizE[i, j] = -matrizA[i, j] / matrizA[i, i];
+                }
+                vectorC[i] = ((Vector)vectorB)[i] / matrizA[i, i];
             }
 
-        return matrizInversa;
-    }
+            IMatriz xAnterior = vectorC;
+            IMatriz x = OperacionIteracion(matrizE, vectorC, xAnterior);
 
-    private static bool CondicionDeValidez(IMatriz matrizA, IMatriz vectorB)
-    {
-        bool matrizCuadrada = matrizA.EsCuadrada();
-        bool mismaDimension = matrizA.SonMultiplicables(vectorB);
-        return matrizCuadrada && mismaDimension;
+            int iteracionesActual = 1;
+            while (CalculoDelError(x, xAnterior) > errorMaximo && iteracionesActual < cantidadDeIteracionesMaximas)
+            {
+                xAnterior = x;
+                x = OperacionIteracion(matrizE, vectorC, x);
+                iteracionesActual++;
+            }
+
+            return x;
+        }
+
+        // matriz A tiene que ser diagonalmente dominante
+        public static IMatriz GaussSeidel(IMatriz matrizA, IMatriz vectorB, int cantidadDeIteracionesMaximas, float errorMaximo)
+        {
+            if (!CondicionDeValidez(matrizA, vectorB))
+                return null;
+
+            uint tamanio = matrizA.Tamanio().Item1;
+            Matriz matrizN = new Matriz(tamanio, tamanio);
+            Matriz matrizP = new Matriz(tamanio, tamanio);
+
+            for (uint i = 0; i < tamanio; i++)
+                for (uint j = 0; j < tamanio; j++)
+                {
+                    if (i <= j)
+                        matrizN[i, j] = matrizA[i, j];
+                    else
+                        matrizP[i, j] = -matrizA[i, j];
+                }
+
+            Matriz matrizNInversa = MatrizTrianguladaSuperiorInversa(matrizN);
+
+            IMatriz matrizE = matrizNInversa.Multiplicar(matrizP);
+            IMatriz vectorC = matrizNInversa.Multiplicar(vectorB);
+
+            IMatriz xAnterior = vectorC;
+            IMatriz x = OperacionIteracion(matrizE, vectorC, xAnterior);
+
+            int iteracionesActual = 1;
+            while (CalculoDelError(x, xAnterior) > errorMaximo && iteracionesActual < cantidadDeIteracionesMaximas)
+            {
+                xAnterior = x;
+                x = OperacionIteracion(matrizE, vectorC, x);
+                iteracionesActual++;
+            }
+
+            return x;
+        }
+
+        private static IMatriz OperacionIteracion(IMatriz matrizE, IMatriz vectorC, IMatriz vectorX)
+        {
+            return vectorC.Sumar(matrizE.Multiplicar(vectorX));
+        }
+
+        private static float CalculoDelError(IMatriz vectorX, IMatriz vectorXAnterior)
+        {
+            return (vectorX.Restar(vectorXAnterior)).ModuloInfinito();
+        }
+
+        private static Matriz MatrizTrianguladaSuperiorInversa(Matriz matriz)
+        {
+            uint tamanio = matriz.Tamanio().Item1;
+            Matriz matrizInversa = new Matriz(tamanio, tamanio);
+
+            for (int x = (int)(tamanio - 1); x >= 0; x--)
+                for (int y = (int)(tamanio - 1); y >= x; y--)
+                {
+                    uint i = (uint)x, j = (uint)y;
+
+                    matrizInversa[i, j] = 1 / matriz[j, j];
+                    if (i != j)
+                        matrizInversa[i, j] *= -matriz[i, j] / matriz[i, i];
+                }
+
+            return matrizInversa;
+        }
+
+        private static bool CondicionDeValidez(IMatriz matrizA, IMatriz vectorB)
+        {
+            bool matrizCuadrada = matrizA.EsCuadrada();
+            bool mismaDimension = matrizA.SonMultiplicables(vectorB);
+            return matrizCuadrada && mismaDimension;
+        }
     }
 }
